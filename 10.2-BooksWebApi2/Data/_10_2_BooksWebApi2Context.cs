d8ff69859d9c5e46c22e5bec9e170c94657bf7f3
ChrysostomosC <153235540+ChrysostomosC@users.noreply.github.com>
2025-03-30 10:55:58 +0200

    upload project

---
+++ 10.2-BooksWebApi2/Data/_10_2_BooksWebApi2Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _10._2_BooksWebApi2.Models;
+
+namespace _10._2_BooksWebApi2.Data
+{
+    public class _10_2_BooksWebApi2Context : DbContext
+    {
+        public _10_2_BooksWebApi2Context (DbContextOptions<_10_2_BooksWebApi2Context> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=localhost;Initial Catalog=BooksWebApi2;Integrated Security=True;Trust Server Certificate=True");
+        }
+
+        public DbSet<_10._2_BooksWebApi2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ 10.2-BooksWebApi2/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace _10._2_BooksWebApi2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
